a2510cbd703353060c05bd12a4c6bea0b5368683
Marion Skinner <marionlskinner@gmail.com>
2023-04-25 23:01:18 -0400

    Add files via upload

---
+++ SkinnerBuddy_SQLFinalProject.sql
@@ -0,0 +1,6 @@
+use wordoccurences;
+insert into word (words) values ('This');
+insert into word (words) values ('is');
+insert into word (words) values ('a');
+insert into word (words) values ('Test');
+
